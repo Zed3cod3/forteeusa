39d23b847e660c2a689ebee0c96afdba7478a610
Zed3cod3 <54313186+Zed3cod3@users.noreply.github.com>
2025-02-12 20:49:26 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -128,34 +128,8 @@ const animationTimeline = () => {
     .to(".idea-3", 0.7, ideaTextTransLeave, "+=2.5")
     .from(".idea-4", 0.7, ideaTextTrans)
     .to(".idea-4", 0.7, ideaTextTransLeave, "+=2.5")
-    .from(
-      ".idea-5",
-      0.7, {
-        rotationX: 15,
-        rotationZ: -10,
-        skewY: "-5deg",
-        y: 50,
-        z: 10,
-        opacity: 0,
-      },
-      "+=1.5"
-    )
-    .to(
-      ".idea-5 span",
-      0.7, {
-        rotation: 90,
-        x: 8,
-      },
-      "+=1.4"
-    )
-    .to(
-      ".idea-5",
-      0.7, {
-        scale: 0.2,
-        opacity: 0,
-      },
-      "+=2"
-    )
+    .from(".idea-5", 0.7, ideaTextTrans)
+    .to(".idea-5", 0.7, ideaTextTransLeave, "+=2.5")
     .staggerFrom(
       ".idea-6 span",
       0.8, {
